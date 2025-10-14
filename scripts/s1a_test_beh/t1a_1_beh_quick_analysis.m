0a4cc15e893bacbce63e5c54a69133c3a68340f9
Fede_C3N <fede.ramirez.t@gmail.com>
2025-10-14 12:15:28 +0200

    New approach for plotting beh results

--- scripts/s1a_test_beh/t1a_1_beh_quick_analysis.m
+++ scripts/s1a_test_beh/t1a_1_beh_quick_analysis.m
@@ -10,57 +10,129 @@ clear
 clc
 close all
 
-load('../../data/conductual/HIQ_022_0_ColorK_MATLAB.mat');
+% Paths
+config.path.beh  = fullfile('..','..','data','beh');
 
-% Get the index of the load
-accuracy = stim.accuracy;
-task_load = stim.setSize;
+% Config
 index_load = [4, 6, 8];
+num_blocks = 5;
+colors = [0    0.4470    0.7410;...
+    0.8500    0.3250    0.0980;...
+    0.9290    0.6940    0.1250;...
+    0.4940    0.1840    0.5560;...
+    0.4660    0.6740    0.1880;...
+    0.3010    0.7450    0.9330;...
+    0.6350    0.0780    0.1840];
 
-% Global accuracy
-global_accuracy = [...
-sum(accuracy(task_load == index_load(1)))/ numel(accuracy(task_load == index_load(1))),...
-sum(accuracy(task_load == index_load(2)))/ numel(accuracy(task_load == index_load(2))),...
-sum(accuracy(task_load == index_load(3)))/ numel(accuracy(task_load == index_load(3)))];
+% Get the subjects' file
+files = dir(fullfile(config.path.beh,'HIQ_0*_ColorK_MATLAB.mat'));
 
-fprintf(1,'Global Index 4 accuracy: %.3f\n', global_accuracy(1))
-fprintf(1,'Global Index 6 accuracy: %.3f\n', global_accuracy(2))
-fprintf(1,'Global Index 8 accuracy: %.3f\n\n', global_accuracy(3))
-
-% Accuracy per block
-figure
-block_accuracy = nan(3,5);
-for iload = 1 : 3
-  current_block_accuracy = accuracy(task_load == index_load(iload));
-  current_block_accuracy = reshape(current_block_accuracy,[],5);
-  block_accuracy(iload,:) = sum(current_block_accuracy,1)/size(current_block_accuracy,1);
-  plot(1:5,block_accuracy(iload,:),'*-')
-  hold on
+% Estimate the beh information of interest
+block_accuracy = nan(numel(index_load),num_blocks,numel(files));
+K = nan(numel(index_load),numel(files));
+for ifile = 1 : numel (files)
+    
+    % Load the current file
+    current_file = fullfile(files(ifile).folder,files(ifile).name);
+    load(current_file);
+    
+    fprintf('WORKING ON %s \n',files(ifile).name);
+    
+    % Get the index of the load
+    accuracy = stim.accuracy;
+    task_load = stim.setSize;
+    
+    % Global accuracy
+    global_accuracy = [...
+        sum(accuracy(task_load == index_load(1)))/ numel(accuracy(task_load == index_load(1))),...
+        sum(accuracy(task_load == index_load(2)))/ numel(accuracy(task_load == index_load(2))),...
+        sum(accuracy(task_load == index_load(3)))/ numel(accuracy(task_load == index_load(3)))];
+    
+    fprintf(1,'Global Index 4 accuracy: %.3f\n', global_accuracy(1))
+    fprintf(1,'Global Index 6 accuracy: %.3f\n', global_accuracy(2))
+    fprintf(1,'Global Index 8 accuracy: %.3f\n\n', global_accuracy(3))
+    
+    % Accuracy per block
+    for iload = 1 : numel(index_load)
+        current_block_accuracy = accuracy(task_load == index_load(iload));
+        current_block_accuracy = reshape(current_block_accuracy,[],5);
+        block_accuracy(iload,:,ifile) = sum(current_block_accuracy,1)/size(current_block_accuracy,1);
+        %         plot(ax1,1:5,block_accuracy(iload,:),'*-')
+    end
+    %     legend('Set 4', 'Set 6', 'Set 8')
+    %     ylim([0 1])
+    %     xticks([1 2 3 4])
+    %     xlabel('Block number')
+    %     ylabel('Accuracy')
+    
+    % K coefficient
+    [current_K,setSize] = computeK(stim);
+    K(:,ifile) = current_K;
+    
+    % Plot
+    %     plot(setSize, K, '-o', 'LineWidth', 2, 'MarkerSize', 8);
+    %     ylim([0 max(K)*1.2]);
+    %     xlim([min(setSize)-0.5, max(setSize)+0.5]);
+    %
+    %     xlabel('Set Size (# of items)');
+    %     ylabel('Estimated K');
+    %     title('Visual Working Memory Capacity (K) by Set Size');
+    %     grid on;
+    %
+    %     % Estética tipo paper
+    %     set(gca, 'FontSize', 12);
+    
+    
+    % Time spent on the task
+    fprintf(1,'Time elapsed: %.3f minutes. \n\n\n\n', stim.triggers.onset(end)/60)
+    
 end
-legend('Set 4', 'Set 6', 'Set 8')
-ylim([0 1])
-xticks([1 2 3 4])
-xlabel('Block number')
-ylabel('Accuracy')
-
-% K coefficient
-[K,setSize] = computeK(stim);
-figure;
-plot(setSize, K, '-o', 'LineWidth', 2, 'MarkerSize', 8);
-ylim([0 max(K)*1.2]);
-xlim([min(setSize)-0.5, max(setSize)+0.5]);
-
-xlabel('Set Size (# of items)');
-ylabel('Estimated K');
-title('Visual Working Memory Capacity (K) by Set Size');
-grid on;
-
-% Estética tipo paper
-set(gca, 'FontSize', 12);
 
 
-% Time spent on the task
-fprintf(1,'Time elapsed: %.3f minutes. \n\n', stim.triggers.onset(end)/60)
+% Plot the information of interest
+% Plot global accuracy
+global_accuracy = squeeze(mean(block_accuracy,2));
+figure
+for ifile = 1 : size(global_accuracy,2)
+    
+    plot(index_load,global_accuracy(:,ifile),'*-')
+    hold on
+    ylim([0 1])
+    xticks(index_load)
+    xlabel('Load')
+    ylabel('Accuracy')
+    
+end
+
+% Plot K
+figure
+for ifile = 1 : size(global_accuracy,2)
+    
+    plot(index_load,K(:,ifile),'*-')
+    hold on
+    xticks(index_load)
+    xlabel('Load')
+    ylabel('K')
+    
+end
+
+
+
+% Plot accuracy per block
+% for iload = 1 : numel(index_load)
+%     figure
+%     current_y = squeeze(block_accuracy(iload,:,:));
+%     plot(1:num_blocks,current_y,'*-','Color',colors(iload,:))
+%     hold on
+%     ylim([0 1])
+%     xticks([1 2 3 4 5])
+%     xlabel('Block number')
+%     ylabel('Accuracy')
+%     title(sprintf('Load %i', index_load(iload)))
+%     
+% end
+
+
 
 
 
@@ -77,33 +149,33 @@ function [K, uniqueSetSizes] = computeK(stim)
 %   K               : vector con valores de K para cada tamaño de set único
 %   uniqueSetSizes  : vector con los tamaños de set correspondientes a cada K
 
-    % Aplanar todas las matrices en vectores
-    setSize = stim.setSize(:);
-    accuracy = stim.accuracy(:);
-    change = stim.change(:);
+% Aplanar todas las matrices en vectores
+setSize = stim.setSize(:);
+accuracy = stim.accuracy(:);
+change = stim.change(:);
 
-    % Identificar los tamaños únicos de set
-    uniqueSetSizes = unique(setSize);
-    K = zeros(size(uniqueSetSizes));
+% Identificar los tamaños únicos de set
+uniqueSetSizes = unique(setSize);
+K = zeros(size(uniqueSetSizes));
 
-    for i = 1:length(uniqueSetSizes)
-        S = uniqueSetSizes(i);
-
-        % Filtrar ensayos que tienen este set size
-        idx = setSize == S;
-
-        acc = accuracy(idx);
-        chg = change(idx);
-
-        % Tasa de aciertos
-        H = mean(acc(chg == 1));
-
-        % Tasa de falsas alarmas
-        F = 1 - mean(acc(chg == 0));
-
-        % Calcular K
-        K(i) = S * (H - F);
-    end
+for i = 1:length(uniqueSetSizes)
+    S = uniqueSetSizes(i);
+    
+    % Filtrar ensayos que tienen este set size
+    idx = setSize == S;
+    
+    acc = accuracy(idx);
+    chg = change(idx);
+    
+    % Tasa de aciertos
+    H = mean(acc(chg == 1));
+    
+    % Tasa de falsas alarmas
+    F = 1 - mean(acc(chg == 0));
+    
+    % Calcular K
+    K(i) = S * (H - F);
+end
 end
 
 
